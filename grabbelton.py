781751982ef9c8bf270d54a305eebf519e1cdab5
Shrosy2511 <99042114@mydavinci.nl>
2022-03-24 10:30:35 +0100

    grabbelton assignment finished

--- grabbelton.py
+++ grabbelton.py
@@ -1,22 +1,33 @@
+from cgitb import text
 from operator import index
 import tkinter as tk
 import random
 
 window = tk.Tk()
 
-lst = ["auto","penis enlargement pills", "good job idiot u pressed a button", "damn what a press", "fuck you tony", "fuck you ezekiel", "im impressed, do you get it", "damn son", "u are gay!", "why u always lyin"]
+lst = ["emotional damage","penis enlargement pills", "good job idiot u pressed a button", "damn what a press", "fuck you tony", "fuck you ezekiel", "im impressed, do you get it", "damn son", "u are gay!", "why u always lyin"]
 
 window.geometry("300x300")
 button = tk.Button(text="click here bitch", bg="black", fg="white")
 button.pack()
 def antwoord():
-    windowAntwoord = tk.Tk()
-    windowAntwoord.geometry("300x300")
-    var = random.choice(lst)
-    label = tk.Label(windowAntwoord, text=var)
-    label.config(bg="black", fg="white", font=32)
-    label.pack()
-    lst.remove(var)
+    if len(lst) == 0:
+        window2 = tk.Tk()
+        window2.geometry("300x300")
+        label1 = tk.Label(window2, text="why are u still clicking don't you have a life?")
+        label1.config(bg='black', fg='white')
+        label1.place(relx=0.5,rely=0.5,anchor='center')
+    else:
+        windowAntwoord = tk.Tk()
+        windowAntwoord.geometry("300x300")
+        var = random.choice(lst)
+        label = tk.Label(windowAntwoord, text=var)
+        label.config(bg="black", fg="white", font=32)
+        label.pack()
+        lst.remove(var)
+
+
+
     
 button.config(command=antwoord)
 
